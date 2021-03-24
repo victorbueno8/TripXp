d152ccee5e36c99f41435a359bec1850930f772e
Victor Bueno <vihto8@gmail.com>
2021-03-23 22:37:36 -0300

    Add base splash screen

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/tripxp/presentation/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package br.edu.ifsp.scl.sdm.tripxp.presentation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import br.edu.ifsp.scl.sdm.tripxp.MainActivity
+import br.edu.ifsp.scl.sdm.tripxp.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    private var TIME_OUT : Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+
+        loadSplashScreen()
+    }
+
+    private fun loadSplashScreen() {
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, TIME_OUT)
+    }
+}
