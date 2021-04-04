4645d499f7b3e6f5eca66128d83237c2a2cc6717
Victor Bueno <vihto8@gmail.com>
2021-04-03 22:34:25 -0300

    Add base base event and users, organizers profile activities

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/tripxp/presentation/OrganizerProfileActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.scl.sdm.tripxp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.scl.sdm.tripxp.R
+
+class OrganizerProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_organizer_profile)
+    }
+}
---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/tripxp/presentation/UserProfileActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.scl.sdm.tripxp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.scl.sdm.tripxp.R
+
+class UserProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_profile)
+    }
+}
