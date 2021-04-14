4bf6f960cb19e1c444448a0ab9cf82b392ea2279
Victor Bueno <vihto8@gmail.com>
2021-04-13 22:37:58 -0300

    Add edit event activity for organizer users

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/tripxp/presentation/organizer/EditEventActivity.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.scl.sdm.tripxp.presentation.organizer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.scl.sdm.tripxp.R
+
+class EditEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_event)
+    }
+}
