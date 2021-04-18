fb28ca7c55417b1359acaa6bb0dec02180f91390
Victor Bueno <vihto8@gmail.com>
2021-04-18 18:35:52 -0300

    Add registration and login connection to firebase authentication

--- app/src/main/java/br/edu/ifsp/scl/sdm/tripxp/presentation/LoginActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/tripxp/presentation/LoginActivity.kt
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import br.edu.ifsp.scl.sdm.tripxp.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        auth = FirebaseAuth.getInstance()
+
         val loginBt: Button = findViewById(R.id.loginBt)
         loginBt.setOnClickListener { view ->
-            // Para activity lista de viagens do usuário/organizador
-            startActivity(Intent(this, MyTripsActivity::class.java))
+            if (emailEt.text.trim().toString().isEmpty())
+                Toast.makeText(this, "Preencha o campo email!", Toast.LENGTH_LONG).show()
+            else if (passwordEt.text.trim().toString().isEmpty())
+                Toast.makeText(this, "Preencha o campo de Senha", Toast.LENGTH_LONG).show()
+            else {
+                signInUser(emailEt.text.trim().toString(), passwordEt.text.trim().toString())
+            }
         }
 
         val registerBt: Button = findViewById(R.id.registerBt)
@@ -23,5 +35,16 @@ class LoginActivity : AppCompatActivity() {
             // Botao Inicia activity de Registrar
             startActivity(Intent(this, RegistrationActivity::class.java))
         }
+    }
+
+    private fun signInUser(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener (this) { task ->
+                    if (task.isSuccessful){
+                        startActivity(Intent(this, MyTripsActivity::class.java))
+                    } else {
+                        Toast.makeText(this, "Erro:" + task.exception, Toast.LENGTH_LONG ).show()
+                    }
+                }
     }
 }
--- app/src/main/java/br/edu/ifsp/scl/sdm/tripxp/presentation/RegistrationActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/tripxp/presentation/RegistrationActivity.kt
@@ -1,12 +1,60 @@
 package br.edu.ifsp.scl.sdm.tripxp.presentation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import br.edu.ifsp.scl.sdm.tripxp.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_registration.*
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        auth = FirebaseAuth.getInstance()
+
+        continuarBt.setOnClickListener {
+            if(registerNameEt.text.trim().toString().isNotEmpty() && registerEmailEt.text.trim().toString().isNotEmpty() &&
+                    registerSenhaEt.text.trim().toString().isNotEmpty() && registerConfirmSenhaEt.text.trim().toString().isNotEmpty()) {
+                        registerUser(registerNameEt.text.trim().toString(), registerEmailEt.text.trim().toString(), registerSenhaEt.text.trim().toString(), registerConfirmSenhaEt.text.trim().toString())
+            } else {
+                Toast.makeText(this, "Preencha os campos acima!", Toast.LENGTH_LONG).show()
+            }
+        }
+
+    }
+
+//    override fun onStart() {
+//        super.onStart()
+//        val user = auth.currentUser
+//
+//        if (user != null) {
+//            startActivity(Intent(this, MyTripsActivity::class.java))
+//        }
+//
+//    }
+
+    private fun registerUser(name: String, email: String, password: String, passwordConfirmation: String) {
+        if(password == passwordConfirmation) {
+            auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if(task.isSuccessful) {
+                            Toast.makeText(this, "Conta Criada!", Toast.LENGTH_LONG).show()
+                            val intent = Intent(this, MyTripsActivity::class.java)
+                            intent.putExtra("name", name)
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, "Erro ao criar conta: " + task.exception, Toast.LENGTH_LONG).show()
+                        }
+                    }
+        } else {
+            Toast.makeText(this, "Confirmação da senha errada!", Toast.LENGTH_LONG).show()
+        }
     }
 }
